9979c5bae38d4635f91cdcd87bdc773168f6db7f
Sungjae.Kwak <sungjae8970@gmail.com>
2022-03-07 17:45:35 +0900

    📝 가독성을 위한 선언문 정리

--- src/components/MainProductList.js
+++ src/components/MainProductList.js
@@ -20,17 +20,20 @@ export default function MainProductList({ $target }) {
     this.setState(products)
     console.log(this.state)
     console.log(this.state[0])
+    
     const $productList = document.createElement('ul')
     const $btnCart = document.createElement('button')
+    
+    $productList.classList.add('product-list');
     $btnCart.classList.add('btn-cart')
     $btnCart.innerHTML = `<img src="./assets/icon-shopping-cart.svg">`
-    $productList.classList.add('product-list');
+
     $page.appendChild($productList)
     $productList.innerHTML = `
       ${this.state.map(product => 
         `
-        <li class='product-item soldout' data-test-data='${product.id}'>
-          <img class='item__img' src="http://35.76.53.28:8080/${product.thumbnailImg}" alt="${product.productName}대표사진">
+        <li class='product-item' data-test-data='${product.id}'>
+          <img class='item__img' src="${PRODUCT_API_IP}/${product.thumbnailImg}" alt="${product.productName}대표사진">
           <h2 class='item__name sl-ellipsis'>${product.productName}
             <button class='item__btn-like'>
               <img src="./assets/icon-heart.svg">
@@ -44,26 +47,39 @@ export default function MainProductList({ $target }) {
         `).join('')}
     `
     $productList.appendChild($btnCart)
-    /*
+
+    /* 안되는코드
     const productList = new MainProductListRender({
       $target : $page,
-      initialState : this.state
+      initialState : this.state 얘가 잘 전달을 못하는 듯?
     })
     */
-    // $productList.addEventListener('click', (e) => console.log(e.target.parentNode.dataset))
     $productList.addEventListener('click', this.handleClickItem)
   }
 
   this.handleClickItem = (e) => {
-    const ID = e.target.parentNode.dataset
+    const ID = e.target.parentNode.dataset // {testData: '3'}
     console.log(ID);
-    const $itemDetailModal = document.createElement('section')
-    $itemDetailModal.innerHTML = `<h1 class='item-detail-modal'>${ID.testData} 제품상세페이지모달입니다.</h1>`
-    $target.appendChild($itemDetailModal)
+
     const productDetail = this.state[ID.testData - 1]
-    console.log(productDetail)
+    const $modalContainer = document.createElement('div')
+    const $itemDetailModal = document.createElement('section')
+    const $closeModalBtn = document.createElement('button')
+    $closeModalBtn.innerHTML = `<img src="./assets/icon-delete.svg">`
+    
+    console.log(Object.keys(productDetail.option)) // 배열을 반환
+    console.log(Object.keys(productDetail.option).length) // 요걸로 옵션 분기처리
+    
+    $modalContainer.classList.add('modal-container')
+    $itemDetailModal.classList.add('modal-content')
+    
+    $modalContainer.appendChild($itemDetailModal)
+    $target.appendChild($modalContainer)
+    
+    $itemDetailModal.innerHTML = `<h1 class='item-detail-modal'>${ID.testData} 제품상세페이지모달입니다.</h1>` // 얘는 아무의미없네
+    
     $itemDetailModal.innerHTML = `
-      <img src="http://35.76.53.28:8080/${productDetail.thumbnailImg}" alt="제품상세이미지">
+      <img src="${PRODUCT_API_IP}/${productDetail.thumbnailImg}" alt="제품상세이미지">
       <h2>${productDetail.productName}</h2>
       <span>택배배송 / 무료배송</span>
       <select name="" id="">
@@ -85,32 +101,27 @@ export default function MainProductList({ $target }) {
         <dl>
           <h2>상품정보</h2>
           <dt>상품 번호</dt>
-          <dd>01010101</dd>
+          <dd>00000${productDetail.id}</dd>
           <dt>재고 수량</dt>
-          <dd>230개</dd>
+          <dd>${productDetail.stockCount}개</dd>
         </dl>
     `
-    const $closeModalBtn = document.createElement('button')
-    $closeModalBtn.innerHTML = `<img src="./assets/icon-delete.svg">`
-    $closeModalBtn.addEventListener('click', closeModal)
     $itemDetailModal.appendChild($closeModalBtn)
+    $closeModalBtn.addEventListener('click', closeModal)
 
     function closeModal(e) {
-      // console.log(e.target.parentNode.parentNode)
-      $itemDetailModal.remove()
+      $modalContainer.remove()
     }
   }
 
   // 왜 이렇게 하면 this.state에 initialState가 안박힐까
-  console.log(this.state) // undefined...
+  console.log(this.state) // undefined
 
   // 페이지 생성시에 상품데이터 긁어오도록
   fetchProducts();
-
 }
 
-// 왜 이렇게 하면 this.state에 initialState가 안박힐까
-// 다른건 문제가 없지만, 상태가 제대라오 안불러와짐
+// 위에서 this.state를 넘겨줌에도 불구하고, 아래에선 초기상태가 세팅이 안됨. 호출스택?과 관련이 있나? 아래는 사용안함
 function MainProductListRender({ $target, initialState }) {
   const $productList = document.createElement('ul')
   $productList.classList.add('productList');
@@ -129,7 +140,7 @@ function MainProductListRender({ $target, initialState }) {
       ${this.state.map(product => 
       `
         <li class='product-item soldout' data-test-data='${product.id}'>
-          <img class='item__img' src="http://35.76.53.28:8080/${product.thumbnailImg}" alt="${product.productName}대표사진">
+          <img class='item__img' src="${PRODUCT_API_IP}/${product.thumbnailImg}" alt="${product.productName}대표사진">
           <h2 class='item__name sl-ellipsis'>${product.productName}
             <button class='item__btn-like'>
               <img src="./assets/icon-heart.svg">
