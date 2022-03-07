e952378df6b9ef7e3f5c71ca0c3b6d11881f6e57
Sungjae.Kwak <sungjae8970@gmail.com>
2022-03-07 13:50:08 +0900

    🩹 Api 외부에서도 사용하도록 밖으로 빼냄

--- src/API.js
+++ src/API.js
@@ -1,28 +1,17 @@
-// 게시물 Data : http://35.76.53.28:8080/mall
-// 쿠폰 Data : http://35.76.53.28:8080/coupon
 // fetch 기본형태는 아래와같다
 // const promise = fetch(resource, [options])
 
-// 강의에서 쓴 fetch예제 
-// const fetchCat = async (text) => {
-//   const OPEN_API_DOMAIN = "https://cataas.com";
-//   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
-//   const responseJson = await response.json();
-//   return `${OPEN_API_DOMAIN}/${responseJson.url}`;
-// };
-
-// const PRODUCT_API_IP = 'http://35.76.53.28:8080/mall';
-// const COUPON_API_IP = 'http://35.76.53.28:8080/coupon';
-
+// 데이터 불러오기 연습용 코드
 // export const request = fetch(PRODUCT_API_DOMAIN)
-// export const request = fetch('http://35.76.53.28:8080/mall')
 //   .then(response => console.log(response))
 //   .then(response => response.json())
 //   .then(json => console.log(json))
 
+const PRODUCT_API_IP = 'http://35.76.53.28:8080/mall';
+const COUPON_API_IP = 'http://35.76.53.28:8080/coupon';
+
 export const requestProduct = async () => {
-  const PRODUCT_API_IP = 'http://35.76.53.28:8080/mall';
   const productResponse = await fetch(PRODUCT_API_IP);
   const productResponseJson = await productResponse.json();
   return productResponseJson;
-}
+};
